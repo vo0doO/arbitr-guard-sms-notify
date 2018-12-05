b0e6447c0b4476850fb27e260c9f1da92b56b538
vo0doo <kirsanov.bvt@gmail.com>
2018-12-05 17:34:56 +0300

    upgrade

--- mail-checker.py
+++ mail-checker.py
@@ -3,9 +3,10 @@
 #
 from __future__ import print_function
 
-import os
-import httplib2
-from apiclient import discovery
+
+import base64
+import email
+
 from apiclient import errors
 from googleapiclient.discovery import build
 from httplib2 import Http
@@ -28,7 +29,7 @@ def get_service():
     service = build('gmail', 'v1', http=creds.authorize(Http()))
     return service
 
-
+#ПОЛУЧАЕМ СПИСОК СООБЩЕНИЙ
 def list_messages(service, user, query=''):
     """Gets a list of messages.
 
@@ -62,10 +63,54 @@ def list_messages(service, user, query=''):
             # TODO: Redirect the user to the authorization URL.
             raise NotImplementedError()
 
+#ПОЛУЧАЕМ ДАННЫЕ КАЖДОГО СООБЩЕНИЯ
+def read_message(service, user_id, msg_id):
+    """Get a Message and use it to create a MIME Message.
+
+     Args:
+       service: Authorized Gmail API service instance.
+       user_id: User's email address. The special value "me"
+       can be used to indicate the authenticated user.
+       msg_id: The ID of the Message required.
+
+     Returns:
+       A MIME Message, consisting of data from Message.
+     """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id, format='raw').execute()
+
+        print(f"Message snippet: {message['snippet']}")
+
+        return message
+    except errors.HttpError as error:
+        print(f"An error occurred: {error}")
+
+def sms_notification(msg_text):
+
+    from twilio.rest import Client
+
+    account_sid = 'AC669d9811cff3e651b8525e353bba5078'
+    auth_token = '1ce7bce90364b673bb7a1683e1bd0222'
+
+    client = Client(account_sid, auth_token)
+
+    client.messages \
+        .create(
+            body=msg_text,
+            from_="+18544005478",
+            to="+79214447344"
+    )
+
+
 
 if __name__ == '__main__':
     import time
+
+    message_box = list()
     while True:
         service = get_service()
         mess_list = list_messages(service, 'me', 'in:anywhere from:guard@arbitr.ru is:unread')
+        for message in mess_list:
+            msg = read_message(service, 'me', message['id'])
+            message_box.append(msg)
         time.sleep(5)
